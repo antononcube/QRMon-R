3ce94b01fd03732d8fd55bca1c578c59adee5380
Anton Antonov <anton.antonov@qio.io>
2019-02-22 22:28:18 -0500

    Added tests for QRMonSeparate(cumulativeQ = FALSE).

--- tests/testthat/test_qr_separation.R
+++ tests/testthat/test_qr_separation.R
@@ -18,17 +18,30 @@ qFracs1 <-
   QRMonSeparateToFractions() %>%
   QRMonTakeValue()
 
-test_that("Correct fractions for temperature data", {
+qFracs2 <-
+  QRMonUnit( dfTemperatureData ) %>%
+  QRMonQuantileRegression( df = 6, degree = 3, quantiles = seq(0.2,0.8,0.2) ) %>%
+  QRMonSeparateToFractions(cumulativeQ = FALSE) %>%
+  QRMonTakeValue()
+
+test_that("Correct cumulative fractions for temperature data", {
   expect_true( abs( qFracs0[["0.2"]] - 0.2) < 0.01 )
   expect_true( abs( qFracs0[["0.4"]] - 0.4) < 0.01 )
   expect_true( abs( qFracs0[["0.6"]] - 0.6) < 0.01 )
   expect_true( abs( qFracs0[["0.8"]] - 0.8) < 0.01 )
 })
 
-test_that("Correct fractions for distribution data", {
+test_that("Correct cumulative fractions for distribution data", {
   expect_true( abs( qFracs1[["0.1"]] - 0.1) < 0.01 )
   expect_true( abs( qFracs1[["0.3"]] - 0.3) < 0.01 )
   expect_true( abs( qFracs1[["0.5"]] - 0.5) < 0.01 )
   expect_true( abs( qFracs1[["0.7"]] - 0.7) < 0.01 )
   expect_true( abs( qFracs1[["0.9"]] - 0.9) < 0.01 )
+})
+
+test_that("Correct non-cumulative fractions for temperature data", {
+  expect_true( abs( qFracs2[["0.2"]] - 0.2) < 0.05 )
+  expect_true( abs( qFracs2[["0.4"]] - 0.2) < 0.05 )
+  expect_true( abs( qFracs2[["0.6"]] - 0.2) < 0.05 )
+  expect_true( abs( qFracs2[["0.8"]] - 0.2) < 0.05 )
 })
